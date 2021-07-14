606e7555d5285180a87baf644100c4e66b0fe7d5
Vishal Manik <manikvishal2000@gmail.com>
2021-07-14 10:07:17 +0530

    Changes by vishal

---
+++ lib/homepage/app_assets.dart
@@ -0,0 +1,13 @@
+class AppAssets {
+  static final String logo = "assets/homepage/logo.png";
+  static final String bg = "assets/homepage/Group 1.png";
+  static final String group = "assets/homepage/Group 2.png";
+  static final String disLikeIcon = "assets/homepage/Icon awesome-heart.png";
+  static final String likeIcon = "assets/homepage/icon3d__128.png";
+
+  static final String campFire = "assets/homepage/A0020.png";
+  static final String djNight = "assets/homepage/pers__2.png";
+  static final String photography = "assets/homepage/icon3d__24.png";
+  static final String competitions = "assets/homepage/icon3d__158.png";
+  static final String headphone = "assets/homepage/icon3d__5.png";
+}
---
+++ lib/login.dart
@@ -0,0 +1,443 @@
+import 'package:flutter/rendering.dart';
+import 'package:bonvoyage/homepage/homepage.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFf5f5f5),
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width / 8),
+        children: [
+          Menu(),
+          // MediaQuery.of(context).size.width >= 980
+          //     ? Menu()
+          //     : SizedBox(), // Responsive
+          Body()
+        ],
+      ),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              _menuItem(title: 'Home'),
+              _menuItem(title: 'About us'),
+              _menuItem(title: 'Contact us'),
+              _menuItem(title: 'Help'),
+            ],
+          ),
+          Row(
+            children: [
+              _menuItem(title: 'Sign In', isActive: true),
+              _registerButton(title: 'Register', isActive: false),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _menuItem({String title = 'Title Menu', isActive = false}) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 75),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Column(
+          children: [
+            Text(
+              '$title',
+              style: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                color: isActive ? Colors.deepPurple : Colors.grey,
+              ),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            isActive
+                ? Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 2),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 6.0,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+            )
+                : SizedBox()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _registerButton({String title = 'Register', isActive = true}) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 75),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Column(
+          children: [
+            Text(
+              '$title',
+              style: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                color: isActive ? Color(0xFF6F35A5) : Colors.grey,
+              ),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            isActive
+                ? Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 2),
+              decoration: BoxDecoration(
+                color: Color(0xFF6F35A5),
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 6.0,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+            )
+                : SizedBox()
+          ],
+        ),
+      ),
+    );
+    // return Container(
+    //   padding: EdgeInsets.symmetric(horizontal: 30, vertical: 8),
+    //   decoration: BoxDecoration(
+    //     color: Colors.white,
+    //     borderRadius: BorderRadius.circular(15),
+    //     boxShadow: [
+    //       BoxShadow(
+    //         color: Colors.grey,
+    //         spreadRadius: 10,
+    //         blurRadius: 12,
+    //       ),
+    //     ],
+    //   ),
+    //   child: Text(
+    //     'Register',
+    //     style: TextStyle(
+    //       fontFamily: 'OpenSans',
+    //       fontWeight: FontWeight.bold,
+    //       color: Colors.black54,
+    //     ),
+    //   ),
+    // );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: 360,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Sign In to \nMy Application',
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Text(
+                    "If you don't have an account you can",
+                    style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        color: Colors.black54, fontWeight: FontWeight.bold),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print(MediaQuery.of(context).size.width);
+                    },
+                    child: Text(
+                      " Register Here!",
+                      style: TextStyle(
+                          fontFamily: 'OpenSans',
+                          color: Color(0xFF6F35A5),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Image.asset(
+                'images/illustration-2.png',
+                width: 300,
+              ),
+            ],
+          ),
+        ),
+
+        MediaQuery.of(context).size.width >= 1300 //Responsive
+            ? Image.asset(
+                'images/illustration-1.png',
+                width: 300,
+              )
+            : SizedBox(),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height / 6),
+          child: Container(
+            width: 320,
+            child: _formLogin(context),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _formLogin(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Color(0xFF6F35A5),
+              width: 2,
+            ),
+            color: Color(0xFFF1E6FF),
+            borderRadius: BorderRadius.circular(15.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black38,
+                blurRadius: 6.0,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: TextFormField(
+          // controller: _emailcontroller,
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(
+            color: Color(0xFF6F35A5),
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.bold,
+          ),
+          cursorColor: Color(0xFF6F35A5),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return '*';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            errorStyle: TextStyle(height: 0),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 12.5),
+            prefixIcon: Icon(
+              Icons.email,
+              color: Color(0xFF6F35A5),
+            ),
+              hintText: 'Enter your Email Address',
+              hintStyle: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                letterSpacing: 0.5,
+              ),
+              labelStyle: TextStyle(fontSize: 12),
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          decoration: BoxDecoration(
+            border: Border.all(
+            color: Color(0xFF6F35A5),
+            width: 2,
+            ),
+            color: Color(0xFFF1E6FF),
+            borderRadius: BorderRadius.circular(15.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black38,
+                blurRadius: 6.0,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: TextFormField(
+            // controller: _passwordcontroller,
+            obscureText: true,
+            style: TextStyle(
+              color: Color(0xFF6F35A5),
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+            ),
+            cursorColor: Color(0xFF6F35A5),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return '*';
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              errorStyle: TextStyle(height: 0),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.symmetric(vertical: 12.5),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Color(0xFF6F35A5),
+              ),
+              hintText: 'Enter your Password',
+              hintStyle: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                letterSpacing: 0.5,
+              ),
+              suffixIcon: Icon(
+                Icons.visibility_off_outlined,
+                color: Colors.grey,
+              ),
+              labelStyle: TextStyle(fontSize: 12),
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black38,
+                blurRadius: 6.0,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: ElevatedButton(
+            child: Container(
+                width: double.infinity,
+                height: 60,
+                child: Center(child: Text("Sign In"))),
+            onPressed: () async {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => HomePage()));
+            },
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xFF6F35A5),
+              onPrimary: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 40),
+        Row(children: [
+          Expanded(
+            child: Divider(
+              color: Colors.grey[300],
+              height: 50,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text("Or continue with"),
+          ),
+          Expanded(
+            child: Divider(
+              color: Colors.grey[400],
+              height: 50,
+            ),
+          ),
+        ]),
+        SizedBox(height: 40),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _loginWithButton(image: 'images/google.png'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _loginWithButton({required String image, bool isActive = false}) {
+    return Container(
+      width: 90,
+      height: 70,
+      decoration: isActive
+          ? BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            spreadRadius: 10,
+            blurRadius: 30,
+          )
+        ],
+        borderRadius: BorderRadius.circular(15),
+      )
+          : BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: Colors.black12),
+      ),
+      child: Center(
+          child: Container(
+            decoration: isActive
+                ? BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(35),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black38,
+                  blurRadius: 6.0,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            )
+                : BoxDecoration(),
+            child: Image.asset(
+              '$image',
+              width: 35,
+            ),
+          )),
+    );
+  }
+}
